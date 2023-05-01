6892cd6e32c3952c9bcc507e16bd278c9c74dc60
DinaDi021 <di.serdyuck0201@gmail.com>
2023-05-01 21:58:23 +0300

    Add lesson 1 script.js

---
+++ lesson1/js/script.js
@@ -0,0 +1,70 @@
+// hw_1.1
+
+let greetings = 'hello';
+console.log(greetings);
+
+let title = 'owu';
+console.log(title);
+
+let linkOne = 'com';
+console.log(linkOne);
+
+let linkTwo ='ua';
+console.log(linkTwo);
+
+let numberA = 1;
+console.log(numberA);
+
+let numberB= 10;
+console.log(numberB);
+
+let numberC = -999;
+console.log(numberC);
+
+let d = 123;
+console.log(d);
+
+let e = 3.14;
+console.log(e);
+
+let f = 2.7;
+console.log(f);
+
+let g = 16;
+console.log(g);
+
+let check = true;
+console.log(check);
+
+let check2 = false;
+console.log(check2);
+
+// hw_1.2
+
+let firstName = 'Diana';
+let middleName = 'Rostislavovna';
+let lastName = 'Ilina';
+
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+// hw_1.3
+
+let a = 100;
+console.log(typeof a, a);
+
+let b = '100';
+console.log(typeof b, b);
+
+let c = true;
+console.log(typeof c, c);
+
+
+let name = prompt('Enter your name')
+console.log(name);
+
+let surname = prompt('Enter your surname')
+console.log(surname);
+
+let age = prompt('Enter your age')
+console.log(age);
